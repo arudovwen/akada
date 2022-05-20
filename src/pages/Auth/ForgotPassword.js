f9d60183a6684fb265d22346c91546adf4066bb1
arudovwen <successahon@gmail.com>
2022-05-20 05:04:01 +0100

    Fixed ui, added forgot paddword, reset pasword, login pages

---
+++ src/pages/Auth/ForgotPassword.js
@@ -0,0 +1,52 @@
+//import akadabg from '../images/background-image.jpg';
+import akadaLogo from "../../images/akada-logo.png";
+import { Link } from "react-router-dom";
+
+import { IoIosArrowDown } from "react-icons/io";
+
+const ForgotPassword = function () {
+  return (
+    <div className="bg-white w-[28rem] pt-8 py-12 px-10 rounded-[30px] right-24 mt-10 absolute scale-in-center">
+      <div className="mt-10 mx-auto">
+        <Link to="/">
+          {" "}
+          <img src={akadaLogo} alt="akada-logo" className="mx-auto mb-10" />
+        </Link>
+      </div>
+
+      <div className="capitalize mb-1 text-text-color text-2xl font-bold">
+        Forgot your password?
+      </div>
+      <p className="mb-5">Don't worry, lets help you set a new one</p>
+
+      <form
+        action="
+        "
+      >
+        <div className=" mb-5">
+          <label htmlFor="" className="text-sm">
+            Enter email
+          </label>
+          <input
+            type="text"
+            className="block border-b-2 w-full h-10 text-xl font-medium"
+            placeholder="Email address"
+          />
+        </div>
+
+        <button className="uppercase bg-text-color w-full text-white py-3 text-lg font-bold rounded-md mb-4 tracking-wider mt-5">
+          <Link to="/verify-account">Send reset</Link>
+        </button>
+
+        <p className="text-center text-sm text-gray-500">
+          Don't have an account?{" "}
+          <Link to="/register" className="text-[#9bab76]">
+            Sign up
+          </Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default ForgotPassword;
