3920d35f77eeeb994aa99931ce8ef3c831982909
arudovwen <successahon@gmail.com>
2022-06-06 13:53:59 +0100

    fix banner

--- src/pages/Landing/Banner.js
+++ src/pages/Landing/Banner.js
@@ -11,16 +11,16 @@ import {Link} from "react-router-dom"
 function Banner() {
   return (
     <div className="relative min-h-[400px] md:min-h-min lg:min-h-[550px] mt-8 py-8 mb-10">
-      <div className="px-6 sm:px-0 sm:w-[93%] grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 h-full md:gap-x-4 lg:gap-x-40 items-center ml-auto">
+      <div className="px-6 lg:px-0 container grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 h-full md:gap-x-4 lg:gap-x-40 items-center mx-auto">
         <div className="md:col-span-1  font-bold text-center md:text-left py-10 h-full flex  items-center relative">
           <div className="z-20 w-full md:w-auto mx-auto md:mx-0">
-            <p className="text-primary text-4xl sm:text-3xl md:text-5xl lg:text-[64px] md:leading-[71px] lg:mb-4 transition tracking-in-expand-fwd delay-300 duration-500">
+            <p className="text-primary text-4xl sm:text-2xl md:text-4xl lg:text-[64px] md:leading-[71px] lg:mb-4 transition tracking-in-expand-fwd delay-300 duration-500">
               Subscribe<span className="hidden md:inline">.</span>
             </p>
-            <p className="text-primary   text-4xl sm:text-3xl md:text-5xl lg:text-[64px] md:leading-[71px]  lg:mb-4 transition tracking-in-expand-fwd">
+            <p className="text-primary   text-4xlsm:text-2xl md:text-4xl lg:text-[64px] md:leading-[71px]  lg:mb-4 transition tracking-in-expand-fwd">
               Select<span className="hidden md:inline">.</span>
             </p>
-            <p className="text-primary  text-4xl sm:text-3xl md:text-5xl lg:text-[64px]  md:leading-[71px] mb-8 transition tracking-in-expand-fwd">
+            <p className="text-primary  text-4xlsm:text-2xl md:text-4xl lg:text-[64px]  md:leading-[71px] mb-8 transition tracking-in-expand-fwd">
               Sponsor<span className="hidden md:inline">.</span>
             </p>
             <Link to="/register">
@@ -43,35 +43,31 @@ function Banner() {
           />
         </div>
         <div className="col-span-1 md:col-span-2">
-          <div className="sm:flex space-x-5">
-            <div className="ml-auto md:ml-0 w-[100%] md:w-[63%] relative overflow-hidden">
-              <img
-                src={akadagirl}
-                alt="banner-img"
-                className="rounded-2xl  md:h-[395px] lg:h-[32rem] w-full lg:block "
-              />
-            </div>
+          <div className="sm:flex space-x-5 md:justify-end">
+            <span>
+              <img src={akadagirl} alt="banner-img" className="rounded-2xl" />
+            </span>
 
-            <div className=" lg:w-[37%] xl:w-[25%] relative overflow-hidden group">
+            <span className=" relative overflow-hidden group">
               <img
                 src={akadaboy}
                 alt="banner-img"
-                className="md:rounded-l-2xl lg:rounded-2xl md:h-[395px] lg:h-[32rem] w-full hidden md:block"
+                className="md:rounded-l-2xl lg:rounded-2xl  hidden md:block"
               />
               <img
                 src={greenrec}
                 alt="banner-img"
                 className="absolute left-0 bottom-12"
               />
-            </div>
+            </span>
 
-            <div className="w-[12%] relative overflow-hidden xl:block  hidden ">
+            <span className=" relative overflow-hidden xl:block  hidden ">
               <img
                 src={akadaboy2}
                 alt="banner-img"
                 // className='object-cover block col-span-1 rounded-xl h-[450px]'
 
-                className="rounded-l-2xl h-[32rem] w-full transition hover:kenburns-top"
+                className="rounded-l-2xl  transition hover:kenburns-top"
               />
               <img
                 src={rec}
@@ -80,7 +76,7 @@ function Banner() {
 
                 className="absolute left-0 bottom-12"
               />
-            </div>
+            </span>
           </div>
         </div>
         <img
